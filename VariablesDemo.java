02e2ac0ebc40447576ec77d4c53328d49bd9fc81
Crispulo2025 <marpalace1@gmail.com>
2025-11-16 15:02:10 +0800

    Add files via upload

---
+++ VariablesDemo.java
@@ -0,0 +1,7 @@
+public class VariablesDemo {
+    public static void main(String[] args) {
+        int age = 20;
+        String name = "Cris";
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
