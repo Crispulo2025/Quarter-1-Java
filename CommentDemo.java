f247d0f2d1511054c7f3608afc7686764796f22c
Crispulo2025 <marpalace1@gmail.com>
2025-11-16 15:39:11 +0800

    Add files via upload

---
+++ CommentDemo.java
@@ -0,0 +1,7 @@
+public class CommentDemo {
+    // Main method: entry point of the program
+    public static void main(String[] args) {
+        // Print a greeting message
+        System.out.println("Hello, students!");
+    }
+}
