a91b627b93efa99cac0185af30ae2f62352540ac
Crispulo2025 <marpalace1@gmail.com>
2025-11-16 15:34:02 +0800

    Add files via upload

---
+++ MenuDemo.java
@@ -0,0 +1,7 @@
+public class MenuDemo {
+    public static void main(String[] args) {
+        System.out.println("1. Start Game");
+        System.out.println("2. Load Game");
+        System.out.println("3. Exit");
+    }
+}
