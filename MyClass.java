765f85a63ff9cd001fcde2c0c10b343d48dd4c11
Crispulo2025 <marpalace1@gmail.com>
2025-11-16 15:21:51 +0800

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    void displayMessage() {
+        System.out.println("Hello from MyClass!");
+    }
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.displayMessage();
+    }
+}
