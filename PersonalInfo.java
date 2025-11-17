091a7a68b741a1734fce066415a9286ab17f92c0
Crispulo2025 <marpalace1@gmail.com>
2025-11-17 09:33:01 +0800

    Add files via upload

---
+++ PersonalInfo.java
@@ -0,0 +1,7 @@
+public class PersonalInfo {
+    public static void main(String[] args) {
+        String name = "Cris Maranan";
+        int age = 25;
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
+}
