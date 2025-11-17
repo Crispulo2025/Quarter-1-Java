f6aa1e055cc8252c9c08db4dbeb244f093807fae
Crispulo2025 <marpalace1@gmail.com>
2025-11-17 09:48:23 +0800

    Add files via upload

---
+++ DifferentTypes.java
@@ -0,0 +1,8 @@
+public class DifferentTypes {
+    public static void main(String[] args) {
+        int age = 25;
+        double height = 5.9;
+        String name = "Cris";
+        System.out.println(name + ", Age: " + age + ", Height: " + height);
+    }
+}
