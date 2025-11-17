89f525df990a6dc9c9cde4c52b0b66bd09eaa264
Crispulo2025 <marpalace1@gmail.com>
2025-11-17 09:58:59 +0800

    Add files via upload

---
+++ CommentsExample.java
@@ -0,0 +1,8 @@
+public class CommentsExample {
+    public static void main(String[] args) {
+        // This prints a greeting
+        System.out.println("Hello!");
+        /* Multi-line comment
+           explaining code */
+    }
+}
