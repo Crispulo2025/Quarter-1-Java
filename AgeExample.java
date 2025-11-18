1d298f3f04bdd793d1008ae97945ec8551d50bd2
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 00:06:08 +0800

    Add files via upload

---
+++ AgeExample.java
@@ -0,0 +1,6 @@
+public class AgeExample {
+    public static void main(String[] args) {
+        int age = 20; // declare and initialize the variable
+        System.out.println("Age: " + age); // print to the console
+    }
+}
