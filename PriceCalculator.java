dd6264b887a0569e30d13cf188731adaf3cbf144
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 00:11:56 +0800

    Add files via upload

---
+++ PriceCalculator.java
@@ -0,0 +1,11 @@
+public class PriceCalculator {
+    public static void main(String[] args) {
+        double price1 = 49.99;
+        double price2 = 15.50;
+        double price3 = 32.25;
+
+        double total = price1 + price2 + price3; // compute total
+
+        System.out.println("Total Price: " + total);
+    }
+}
