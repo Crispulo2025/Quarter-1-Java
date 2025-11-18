36aff1c0c916ca6dfa795a8e6a6b22653b2f1184
Crispulo2025 <marpalace1@gmail.com>
2025-11-18 16:35:37 +0800

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,13 @@
+public class Student {
+    int age;          // instance variable
+    static String school = "TIP"; // class variable
+
+    public static void main(String[] args) {
+        int score = 95; // local variable
+        System.out.println("Score: " + score);
+        Student s = new Student();
+        s.age = 18;
+        System.out.println("Student Age: " + s.age);
+        System.out.println("School: " + Student.school);
+    }
+}
