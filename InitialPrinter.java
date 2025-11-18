d7ba5ed75ea645b75d5e1c3eee55f53ae50dadba
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 00:16:19 +0800

    Add files via upload

---
+++ InitialPrinter.java
@@ -0,0 +1,6 @@
+public class InitialPrinter {
+    public static void main(String[] args) {
+        char initial = 'C'; // replace with your own initial
+        System.out.println("My first initial is: " + initial);
+    }
+}
