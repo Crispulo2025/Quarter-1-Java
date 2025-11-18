28b20964fbada2ee8ebc68d407a00fe591eb0daf
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 00:21:18 +0800

    Add files via upload

---
+++ PassStatus.java
@@ -0,0 +1,11 @@
+public class PassStatus {
+    public static void main(String[] args) {
+        boolean passed = true; // change to false to test the other message
+
+        if (passed) {
+            System.out.println("The student passed the exam.");
+        } else {
+            System.out.println("The student did not pass the exam.");
+        }
+    }
+}
