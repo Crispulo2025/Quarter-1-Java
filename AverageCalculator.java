a6b31a044909841098e037b2edd833fbdde690aa
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 08:38:42 +0800

    Add files via upload

---
+++ AverageCalculator.java
@@ -0,0 +1,12 @@
+public class AverageCalculator {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+
+        // calculate average with explicit conversion to double
+        double average = (num1 + num2 + num3) / 3.0;
+
+        System.out.println("Average: " + average);
+    }
+}
