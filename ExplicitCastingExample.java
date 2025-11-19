66807b9dfc12d657bcc97ede6afdf6a9b3d80326
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 09:55:03 +0800

    Add files via upload

---
+++ ExplicitCastingExample.java
@@ -0,0 +1,9 @@
+public class ExplicitCastingExample {
+    public static void main(String[] args) {
+        double myDouble = 9.78;           // double variable
+        int myInt = (int) myDouble;       // explicit type casting to int
+
+        System.out.println("Original double value: " + myDouble);
+        System.out.println("Value after explicit casting to int: " + myInt);
+    }
+}
