7221f7e641ac74c3dadcd14e3f7283fe0a5b3f5d
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 09:47:44 +0800

    Add files via upload

---
+++ ImplicitCastingExample.java
@@ -0,0 +1,9 @@
+public class ImplicitCastingExample {
+    public static void main(String[] args) {
+        int numInt = 25; // int variable
+        double numDouble = numInt; // implicit type casting from int to double
+
+        System.out.println("Integer value: " + numInt);
+        System.out.println("Double value after implicit casting: " + numDouble);
+    }
+}
