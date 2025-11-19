62c628ae62a7567a5b7d68b8e7f33093be016141
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 11:04:02 +0800

    Add files via upload

---
+++ StringConcatenationExample.java
@@ -0,0 +1,11 @@
+public class StringConcatenationExample {
+    public static void main(String[] args) {
+        String firstName = "Cris";
+        String lastName = "Maranan";
+
+        // concatenate the two strings
+        String fullName = firstName + " " + lastName;
+
+        System.out.println("Full Name: " + fullName);
+    }
+}
