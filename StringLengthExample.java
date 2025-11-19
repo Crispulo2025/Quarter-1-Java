51632168a23b110aaa0d3c6e6688c61e5b1d88f8
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 09:39:03 +0800

    Add files via upload

---
+++ StringLengthExample.java
@@ -0,0 +1,9 @@
+public class StringLengthExample {
+    public static void main(String[] args) {
+        String text = "Hello, World!"; // declare a String variable
+
+        int length = text.length(); // get the length of the string
+
+        System.out.println("The length of the string is: " + length);
+    }
+}
