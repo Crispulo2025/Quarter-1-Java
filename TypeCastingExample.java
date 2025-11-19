a414392d76e20375158df40729b8420e7938824b
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 08:31:01 +0800

    Add files via upload

---
+++ TypeCastingExample.java
@@ -0,0 +1,9 @@
+public class TypeCastingExample {
+    public static void main(String[] args) {
+        double myDouble = 9.78;
+        int myInt = (int) myDouble; // explicit type casting
+
+        System.out.println("Original double value: " + myDouble);
+        System.out.println("Converted int value: " + myInt);
+    }
+}
