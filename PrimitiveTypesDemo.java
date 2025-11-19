2ba8eff86ab9b20a2081437c68bccc4d12d034fe
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 09:24:12 +0800

    Add files via upload

---
+++ PrimitiveTypesDemo.java
@@ -0,0 +1,25 @@
+public class PrimitiveTypesDemo {
+    public static void main(String[] args) {
+        // primitive data types
+        byte b = 10;
+        short s = 200;
+        int i = 1500;
+        long l = 100000L;
+
+        float f = 3.14f;
+        double d = 99.99;
+
+        char c = 'A';
+        boolean flag = true;
+
+        // printing values
+        System.out.println("byte value: " + b);
+        System.out.println("short value: " + s);
+        System.out.println("int value: " + i);
+        System.out.println("long value: " + l);
+        System.out.println("float value: " + f);
+        System.out.println("double value: " + d);
+        System.out.println("char value: " + c);
+        System.out.println("boolean value: " + flag);
+    }
+}
