7df8120e04c443db10232a1dea8cb391c68724be
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 10:16:19 +0800

    Add files via upload

---
+++ BooleanExample.java
@@ -0,0 +1,11 @@
+public class BooleanExample {
+    public static void main(String[] args) {
+        boolean isRaining = true; // boolean variable
+
+        if (isRaining) {
+            System.out.println("Take an umbrella. It's raining!");
+        } else {
+            System.out.println("No umbrella needed. It's not raining.");
+        }
+    }
+}
