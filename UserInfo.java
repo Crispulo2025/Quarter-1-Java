928afbb79e080799e622ed57e3b136d5c44d7423
Crispulo2025 <marpalace1@gmail.com>
2025-11-19 10:21:05 +0800

    Add files via upload

---
+++ UserInfo.java
@@ -0,0 +1,9 @@
+public class UserInfo {
+    public static void main(String[] args) {
+        String name = "Cris"; // store name
+        int age = 20;         // store age
+
+        // print a formatted sentence
+        System.out.println("My name is " + name + " and I am " + age + " years old.");
+    }
+}
