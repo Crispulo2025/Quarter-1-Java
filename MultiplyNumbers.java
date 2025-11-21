bda956d4cde0447e2dd883e6c992eab8c654e018
Crispulo2025 <marpalace1@gmail.com>
2025-11-21 14:17:20 +0800

    Add files via upload

---
+++ MultiplyNumbers.java
@@ -0,0 +1,16 @@
+public class MultiplyNumbers {
+    public static void main(String[] args) {
+
+        // Declare the first number
+        int num1 = 8;
+
+        // Declare the second number
+        int num2 = 5;
+
+        // Multiply the two numbers
+        int product = num1 * num2;
+
+        // Print the result
+        System.out.println("The product is: " + product);
+    }
+}
