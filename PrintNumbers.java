2e8f0aea3f3a9bb704b869dcfe95cc5e86b1b25e
Crispulo2025 <marpalace1@gmail.com>
2025-11-21 15:17:58 +0800

    Add files via upload

---
+++ PrintNumbers.java
@@ -0,0 +1,12 @@
+public class PrintNumbers {
+
+    public static void main(String[] args) {
+
+        // Using a for loop to print numbers from 1 to 5
+        // The loop starts with i = 1, continues while i <= 5,
+        // and increments i by 1 in each iteration.
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
