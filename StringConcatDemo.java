a5a93c37bcbf4c1051df45a9fcf262975e8fd65b
Crispulo2025 <marpalace1@gmail.com>
2025-11-21 14:56:12 +0800

    Add files via upload

---
+++ StringConcatDemo.java
@@ -0,0 +1,18 @@
+public class StringConcatDemo {
+
+    public static void main(String[] args) {
+
+        // First string variable
+        String firstName = "Cris";
+
+        // Second string variable
+        String lastName = "Maranan";
+
+        // Concatenate the two strings using the + operator.
+        // String concatenation joins two or more strings into one continuous string.
+        String fullName = firstName + " " + lastName;
+
+        // Print the result of the string concatenation
+        System.out.println("Full Name: " + fullName);
+    }
+}
