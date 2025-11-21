c541fab809d754bef1a0e5e90207c07768b0c7b1
Crispulo2025 <marpalace1@gmail.com>
2025-11-21 14:21:38 +0800

    Add files via upload

---
+++ CommentDemoProgram.java
@@ -0,0 +1,24 @@
+public class CommentDemoProgram {
+
+    public static void main(String[] args) {
+
+        /* 
+         This program demonstrates the use of:
+         - Single-line comments
+         - Multi-line comments
+         It also performs a simple addition of two numbers.
+        */
+
+        // Declare the first number
+        int a = 12;
+
+        // Declare the second number
+        int b = 8;
+
+        // Add the two numbers
+        int sum = a + b;
+
+        // Print the result to the screen
+        System.out.println("The sum is: " + sum);
+    }
+}
