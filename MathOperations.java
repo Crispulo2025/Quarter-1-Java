4c164e7ca83bbf702ad89abbab582d1a59276014
Crispulo2025 <marpalace1@gmail.com>
2025-11-21 15:52:48 +0800

    Add files via upload

---
+++ MathOperations.java
@@ -0,0 +1,14 @@
+// Class containing the method to be called
+public class MathOperations {
+
+    /**
+     * This method adds two integers and returns the result.
+     *
+     * @param a first number
+     * @param b second number
+     * @return sum of a and b
+     */
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+}
