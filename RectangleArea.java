5149087fbdbca64a8cb47d01dae707230f86dfe9
Crispulo2025 <marpalace1@gmail.com>
2025-11-21 14:52:12 +0800

    Add files via upload

---
+++ RectangleArea.java
@@ -0,0 +1,17 @@
+public class RectangleArea {
+
+    public static void main(String[] args) {
+
+        // Declare the length of the rectangle
+        double length = 10.5;
+
+        // Declare the width of the rectangle
+        double width = 4.2;
+
+        // Multiply length by width to compute the area
+        double area = length * width;
+
+        // Print the calculated area
+        System.out.println("The area of the rectangle is: " + area);
+    }
+}
