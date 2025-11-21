cd94a8b88abb3b6ded9bc668be3147b8d607ab3c
Crispulo2025 <marpalace1@gmail.com>
2025-11-21 14:29:58 +0800

    Add files via upload

---
+++ PrintNameExample.java
@@ -0,0 +1,8 @@
+public class PrintNameExample {
+
+    public static void main(String[] args) {
+
+        // System.out.println() is used to display text on the screen and automatically move to the next line.
+        System.out.println("Cris Maranan");
+    }
+}
